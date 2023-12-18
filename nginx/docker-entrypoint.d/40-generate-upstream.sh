7fa91ff616979c63828a71fbec1b1c0eabd708c2
user <user@localhost.localdomain>
2023-12-18 15:26:42 -0500

    allow setting of different algorithms, server goes down if upstream is offline

--- nginx/docker-entrypoint.d/40-generate-upstream.sh
+++ nginx/docker-entrypoint.d/40-generate-upstream.sh
@@ -12,11 +12,25 @@ backend+=($(env | grep HOST_BACKEND | sed 's/HOST_BACKEND_.=//g'))
 
 COUNTER=0
 
+# Create mappings
 for FRONTEND in $(env | grep HOST_FRONTEND); do
 
-        echo "    ${frontend[$COUNTER]} ${backend[$COUNTER]};" >> /etc/nginx/conf.d/upstream-map.conf
+        echo "    ${frontend[$COUNTER]} ${frontend[$COUNTER]}-backend;" >> /etc/nginx/conf.d/upstream-map.conf
         COUNTER=$((COUNTER+1))
 
 done
 
-echo "}" >> /etc/nginx/conf.d/upstream-map.conf
+echo -e "}\n" >> /etc/nginx/conf.d/upstream-map.conf
+
+COUNTER=0
+
+# Create upstreams
+for FRONTEND in $(env | grep HOST_FRONTEND); do
+
+        echo "upstream ${frontend[$COUNTER]}-backend {" >> /etc/nginx/conf.d/upstream-map.conf
+        echo "    $ALGORITHM;" >> /etc/nginx/conf.d/upstream-map.conf
+        echo "    server ${backend[$COUNTER]};" >> /etc/nginx/conf.d/upstream-map.conf
+        echo -e "}\n" >> /etc/nginx/conf.d/upstream-map.conf
+        COUNTER=$((COUNTER+1))
+
+done
